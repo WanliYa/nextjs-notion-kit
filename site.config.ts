b24c81e0754ccd8aa1041d1dc1cdbb98d987ee1c
WanliYa <139261737+WanliYa@users.noreply.github.com>
2023-08-12 12:47:30 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -50,7 +50,7 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'false'
+  navigationStyle: 'defult'
   // navigationStyle: 'custom',
   // navigationLinks: [
   //   {
